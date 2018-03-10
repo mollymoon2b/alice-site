46ac87df8b78c477c43d2defc1cbed3f7950fc35
Alice <mollymoon2b@gmail.com>
2018-03-10 18:09:15 +0100

    finish commit

--- js/menu.js
+++ js/menu.js
@@ -6,19 +6,17 @@ function creatLink(elem, href, text, className) {
     elem.href = href;
 }
 
-function createContentMenu(width, height) {
+function createContentMenu() {
     const containerLink = document.createElement('div');
     const linkWork = document.createElement('a');
     const linkAbout = document.createElement('a');
     const linkContact = document.createElement('a');
 
     containerLink.className = 'menu-link';
-    containerLink.style.height = height;
-    containerLink.style.width = width;
 
     creatLink(linkWork, 'index.html', 'WORK', 'menu-link__item');
     creatLink(linkAbout,  'about.html', 'ABOUT', 'menu-link__item');
-    creatLink(linkContact,  'contact.html', 'CONTACT', 'menu-link__item');
+    creatLink(linkContact,  'mailto:helfer.alice@gmail.com', 'CONTACT', 'menu-link__item');
 
     containerLink.appendChild(linkWork);
     containerLink.appendChild(linkAbout);
@@ -28,7 +26,13 @@ function createContentMenu(width, height) {
 }
 
 if (menu) {
-    menu.addEventListener('click', function() {
+    menu.addEventListener('click', function(e) {
+			if (e.target.className === 'menu-stripe') {
+				e.target.parentElement.style.display = "none";
+			} else {
+				e.target.style.display = "none";
+			}
+			console.log(e.target);
 				document.body.style.overflow = 'hidden';
         const divMenu =  document.getElementsByClassName('menu-content')[0]
             ? document.getElementsByClassName('menu-content')[0]
@@ -47,32 +51,26 @@ if (menu) {
         divMenu.className = 'menu-content';
         divMenu.style.top = scrollPosition === 0 ? 0 :`${document.documentElement.scrollTop}px`;
         divMenu.style.left = 0;
-        divMenu.style.display = 'block';
+        divMenu.style.display = 'flex';
 
         closeButton.className = 'menu-close-button js-close-menu';
         closeButton.addEventListener('click', function () {
 						document.body.style.overflow = 'initial';
             divMenu.style.display = 'none';
+						e.target.style.display = "block";
         });
         if (!document.getElementsByClassName('menu-link')[0]) {
-            divMenu.appendChild(createContentMenu(width, height));
+            divMenu.appendChild(createContentMenu());
         }
         divMenu.appendChild(closeButton);
         document.body.appendChild(divMenu);
     });
 }
 
-/*const footer = document.getElementsByClassName('footer-image')[0];
+const footer = document.getElementsByClassName('footer-image')[0];
 
 if (footer) {
 	footer.addEventListener('click', function() {
-		let scrollPosition = document.documentElement.scrollTop;
-		var refreshId = setInterval(function() {
-			document.body.scrollTop = 0;
-			document.documentElement.scrollTop = scrollPosition--;
-			if (document.documentElement.scrollTop < 0) {
-				clearInterval(refreshId);
-			}
-		}, 1);
+		document.documentElement.scrollTop = 0;
 	})
-}*/
+}
